27a0b97873c2216bfccc9f7764e7bbc08d880c9b
YuvrajPSThakur <ythakur@ec.iitr.ac.in>
2021-09-22 16:07:31 +0530

    html and JS

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
+const n = parseInt(
+    prompt(
+        "Enter the value of N:"
+    )
+);
+
+let number1 = 0 , number2 = 1;
+let newNumber ;
+
+console.log("Fibonacci Sequence");
+
+if (n<1){
+    console.log("Number should be greater than 0")
+
+}else {
+    for(let i =1;i<=n ;i++){
+        console.log(number1);
+        newNumber = number1 + number2;
+        number1 = number2;
+        number2 = newNumber;
+    }
+}
+
 
